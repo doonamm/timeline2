ac5e0a9d3f92f79ec400e915b29f8f6d21b1da01
Nam Ant <83125103+doonamm@users.noreply.github.com>
2021-08-17 13:25:30 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 window.addEventListener('load', function(){
     const milestones = document.querySelectorAll('.milestone');
+    const icons = document.querySelectorAll('.icon i');
     const scrollTopBtn = document.getElementById('scroll-top');
-    
+
     scrollTopBtn.addEventListener('click', scrollToTop);
     document.addEventListener('scroll', scrolling);
     document.addEventListener('resize', scrolling);
+    icons.forEach(el => el.addEventListener('click', scrollTo))
     scrolling();
 
     function scrolling(){
+      
         const {scrollTop, offsetHeight} = document.documentElement;
         milestones.forEach(el => {
             if(el.offsetTop <= scrollTop + offsetHeight*66/100){
@@ -22,6 +25,10 @@ window.addEventListener('load', function(){
                 el.classList.remove('show');
             }
         })
+    }
+    function scrollTo(e){
+        const posY = document.documentElement.scrollTop + e.currentTarget.getBoundingClientRect().top;
+        window.scrollTo({top: posY, behavior: 'smooth'});
     }
     function scrollToTop(){
         window.scrollTo({top: 0, behavior: 'smooth'});
